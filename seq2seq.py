5cedd9035326c6cf22eea3e950b28b562257d457
dylandrover <dylandrover@gmail.com>
2016-12-06 15:38:25 -0500

    added graphical structure to network, to allow for classification and seq. generation

--- seq2seq.py
+++ seq2seq.py
@@ -1,20 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import pandas as pd 
 
 from sklearn.neighbors import NearestNeighbors, DistanceMetric
 from scipy.spatial.distance import cosine
 
-from keras.layers import Dense, LSTM, Embedding
+from keras.layers import Dense, LSTM, Embedding, merge, Input
 from keras import objectives
 from keras import optimizers.Adam
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+
 
 # Input hullaballoo
-GLOVE_FILE = "wordvecs/glove.6B.100d.txt"
-EMBEDDING_DIM = 100
+reddit_df = pd.read_hdf("")
+EMBEDDING_DIM = 50
 INPUT_DIR = ""
 
+X_train = reddit_df['parent']
+y_train = reddit_df['child']
+
+y_train_class = reddit_df['subreddit']
+
+print(len(X_train))
+
+"""
 # Create dictionary of all words in and vectors
 embeddings_index = {}
 f = open(GLOVE_FILE)
@@ -43,6 +55,10 @@ model.add(Embedding(len(word_index) + 2,
                     weights=[embedding_matrix],
                     mask_zero=True,
                     trainable=False)
+
+# Create LSTM encoder and decoder for sequence of Word Vectors
+model = Sequential()
+
 model.add(LSTM(64, input_dim=EMBEDDING_DIM, 
                activation='relu', 
                dropout_W=0.1, dropout_U=0.1, 
@@ -57,6 +73,31 @@ model.compile(loss='cosine_proximity', optimizer=adam)
 model.fit(X_train, X_train, 
           n_epoch=100,
           batch_size=50, validation_split=0.1)
+"""
+
+# Alternate, Functional API for model
+
+input_layer = LSTM(64, input_dim=EMBEDDING_DIM, 
+                   activation='relu', 
+                   dropout_W=0.1, dropout_U=0.1, 
+                   return_sequences=True, name='input_layer')
+
+output_layer = LSTM(100, activation='relu', 
+                    dropout_W=0.1, dropout_U=0.1, 
+                    return_sequences=True, name='output_layer')(input_layer)
+
+x = Dense(128, activation='relu')(input_layer)
+class_layer = Dense(128, activation='softmax', name='class_layer')(x)
+
+model = Model(input=[input_layer], output=[output_layer, class_layer])
+
+model.compile(optimizer=adam, 
+              loss={'output_layer': 'cosine_proximity', 'class_layer': 'categoircal_crossentropy'}, 
+              loss_weights=[1., 1.])
+
+model.fit({'input_layer': X_train}, 
+          {'output_layer': y_train, 'class_layer': y_train_class},
+          nb_epochs=100, batch_size=32)
 
 # Create NN for LSTM output to get the closest approximate word vector 
 # This may be supplanted by a categorical cross entropy approach (usual way)
